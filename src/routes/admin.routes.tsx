9c787e71f1ecbf5e65a5f1f57fb5a62fb1729bed
Jayedjb765 <jayedjb76@gmail.com>
2025-04-12 16:15:34 +0600

    adding redux

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -79,28 +79,6 @@ export const AdminSidebarItem = adminPaths.reduce(
   },
   []
 );
-// export const AdminSidebarItems = adminPaths.reduce(
-//   (acc: TSidebarItems[], item) => {
-//     if (item.path && item.name) {
-//       acc.push({
-//         key: item.name,
-//         label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-//       });
-//     }
-//     if (item.children) {
-//       acc.push({
-//         key: item.name,
-//         label: item.name,
-//         children: item.children.map((child) => ({
-//           key: child.name,
-//           label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-//         })),
-//       });
-//     }
-//     return acc;
-//   },
-//   []
-// );
 
 // export const adminPaths = [
 //   {
