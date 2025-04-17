faa1a411a629eb8bfb57cd27dbe1fc1402d4c6c4
Jayedjb765 <jayedjb76@gmail.com>
2025-04-17 21:38:11 +0600

    handeling academic semester

---
+++ src/redux/features/academicSemester/academicSemesterApi.tsx
@@ -0,0 +1,14 @@
+import { baseApi } from "@/redux/api/basAPi";
+
+const academicSemesterApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllSemesters: builder.query({
+      query: () => ({
+        url: "/academic-semesters",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const {} = academicSemesterApi;
